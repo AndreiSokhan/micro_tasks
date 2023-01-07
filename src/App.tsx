28ac8c4a20bf6091f3d61d27046dcdd2546bd012
Andrei <sokhanpageproofs@gmail.com>
2023-01-07 16:43:12 +0300

    filter end

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import {Header} from "./site/Header";
 import {Body} from "./site/Body";
 import {Footer} from "./site/Footer";
 import {NewComponent} from "./components/NewComponent";
+import {NewFilter} from "./components/NewFilter";
+
+ type filterType = 'All' | 'Dollars' | 'Rubls'
 
 function App() {
    let [students, setStudents] = useState([
@@ -23,12 +26,57 @@ function App() {
       {id: 14, name: "JFJDfo", age: 53}
    ]);
 
+   const [money, setMoney] = useState([
+      { banknots: 'Dollars', value: 100, number: ' a1234567890' },
+      { banknots: 'Dollars', value: 50, number: ' z1234567890' },
+      { banknots: 'Rubls', value: 100, number: ' w1234567890' },
+      { banknots: 'Dollars', value: 100, number: ' e1234567890' },
+      { banknots: 'Dollars', value: 50, number: ' c1234567890' },
+      { banknots: 'Rubls', value: 100, number: ' r1234567890' },
+      { banknots: 'Dollars', value: 50, number: ' x1234567890' },
+      { banknots: 'Rubls', value: 50, number: ' v1234567890' },
+   ])
+
+   const [filter,setfilter]=useState<filterType>('All')
+
+   let currentMoney=money
+   if (filter==='Rubls') {
+      currentMoney=money.filter(filteredMoney=>filteredMoney.banknots==='Rubls')
+   } if (filter==='Dollars'){
+      currentMoney=money.filter(filteredMoney=>filteredMoney.banknots==='Dollars')
+   }
+
+   const onClickFilterHandler=(nameButton:filterType)=>{
+      setfilter(nameButton)
+   }
+
    return (
       <>
+         <NewFilter
+            money={money}
+            currentMoney={currentMoney}
+            onClickFilterHandler={onClickFilterHandler}
+         />
+         {/*<ul>*/}
+         {/*   {currentMoney.map((objFromMoneyArr,index)=>{*/}
+         {/*      return(*/}
+         {/*         <li key={index}>*/}
+         {/*            <span> {objFromMoneyArr.banknots} </span>*/}
+         {/*            <span> {objFromMoneyArr.value} </span>*/}
+         {/*            <span> {objFromMoneyArr.number }</span>*/}
+         {/*         </li>*/}
+         {/*      )*/}
+         {/*   })}*/}
+         {/*</ul>*/}
+         {/*<div style={{marginLeft: '35 px'}}>*/}
+         {/*   <button onClick={()=>{onClickFilterHandler('All')}}>All</button>*/}
+         {/*   <button onClick={()=>{onClickFilterHandler('Rubls')}}>Rubls</button>*/}
+         {/*   <button onClick={()=>{onClickFilterHandler('Dollars')}}>Dollars</button>*/}
+         {/*</div>*/}
          {/*<Header title={'New Header'}/>*/}
          {/*<Body titleForBody={'New Body'}/>*/}
          {/*<Footer titleForFooter={'New Footer'}/>*/}
-         <NewComponent students={students}/>
+         {/*<NewComponent students={students}/>*/}
       </>
    )
 }
